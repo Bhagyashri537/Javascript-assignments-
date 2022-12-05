137a56f75d5d58f2e7cfa10b77841e53aefdcabf
Bhagyashri537 <bhagyashrideoghare116@gmail.com>
2022-12-05 19:42:56 +0530

    next file

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-//console.dir(document);
-/*console.log(document.URL);
-console.log(document.title)
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.forms);
-console.log(document.links)
-*/
+var li = document.getElementsByClassName('list-group-item')
+console.log(li)
+li[1].style.backgroundColor = "red";
 
-//GET ELEMENT BY ID
-
- //document.getElementById("add-item").style.color = "green"
- //document.getElementById("add-item").style.fontWeight = "900"
-// document.getElementById("header-title").style.borderBottom = " solid black";
-
-//GET ELEMENT BY CLASS NAME
-
-var items = document.getElementsByClassName('list-group-item')
-console.log(items)
-items[2].style.backgroundColor = "green";
-
-for(var i=0; i<items.length; i++)
+for(var i=0; i<li.length; i++)
 {
-    items[i].style.fontWeight = "bold";
+    li[i].style.fontWeight = "bold";
 }
-
 
