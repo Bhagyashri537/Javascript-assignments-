74ed2860f69990f67177ec1bb3d0412b481e176c
Bhagyashri537 <bhagyashrideoghare116@gmail.com>
2022-12-05 19:26:44 +0530

    newfile

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ console.log(document.links)
 
 //GET ELEMENT BY ID
 
- document.getElementById("add-item").style.color = "green"
- document.getElementById("add-item").style.fontWeight = "900"
- document.getElementById("header-title").style.borderBottom = " solid black";
+ //document.getElementById("add-item").style.color = "green"
+ //document.getElementById("add-item").style.fontWeight = "900"
+// document.getElementById("header-title").style.borderBottom = " solid black";
+
+//GET ELEMENT BY CLASS NAME
+
+var items = document.getElementsByClassName('list-group-item')
+console.log(items)
+items[2].style.backgroundColor = "green";
+
+for(var i=0; i<items.length; i++)
+{
+    items[i].style.fontWeight = "bold";
+}
+
+
