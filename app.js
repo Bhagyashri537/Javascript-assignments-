c7cf814eb50eca60c784b4d8333ec4fb0c64a9c1
Bhagyashri537 <bhagyashrideoghare116@gmail.com>
2022-12-05 18:05:36 +0530

    add items

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello")
+//console.dir(document);
+/*console.log(document.URL);
+console.log(document.title)
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.forms);
+console.log(document.links)
+*/
+
+//GET ELEMENT BY ID
+
+ document.getElementById("add-item").style.color = "green"
+ document.getElementById("add-item").style.fontWeight = "900"
+ document.getElementById("header-title").style.borderBottom = " solid black";
