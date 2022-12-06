88e84ba1bdb05dba292d19a81696d708782e17ac
Bhagyashri537 <bhagyashrideoghare116@gmail.com>
2022-12-06 13:27:14 +0530

    new

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var li = document.getElementsByClassName('list-group-item')
-console.log(li)
-li[1].style.backgroundColor = "red";
+//var seconditem = document.querySelector(".list-group-item:nth-child(2)")
+//seconditem.style.background = "green"
+ 
+//var thirditem = document.querySelector(".list-group-item:nth-child(3)")
+//thirditem.style.display = "none"
 
-for(var i=0; i<li.length; i++)
-{
-    li[i].style.fontWeight = "bold";
+//queryselecterall
+
+var item = document.querySelectorAll(".list-group-item")
+console.log(item);
+item[1].style.color = "green";
+
+var odd = document.querySelectorAll("li:nth-Child(odd)")
+for(var i=0; i<odd.length; i++){
+    odd[i].style.background = "green"
 }
-
