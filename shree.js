fbd297213ce1ec5a71d49afca2ecabf4e83328b1
Bhagyashri537 <bhagyashrideoghare116@gmail.com>
2022-12-18 11:43:32 +0530

    bind

--- shree.js
+++ shree.js
@@ -1,8 +1,9 @@
 
 var obj = {num : 5}
 var multiplyNum = function(a, b, c){
-    return this.num * a *b*c
+    return this.num * a * b * c
 };
-console.log(multiplyNum.apply(obj,[1,2,3]))
-
+var arr = [1,2,3]
+var bound = multiplyNum.bind(obj);
+console.log(bound(1,2,3));
 
