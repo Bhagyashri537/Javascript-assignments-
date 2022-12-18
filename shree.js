d6b59eec766a4428f6eed290f7093eaf58fdcea4
Bhagyashri537 <bhagyashrideoghare116@gmail.com>
2022-12-18 11:50:04 +0530

    passing an object

--- shree.js
+++ shree.js
@@ -1,9 +1,9 @@
 
-var obj = {num : 5}
-var multiplyNum = function(a, b, c){
-    return this.num * a * b * c
-};
-var arr = [1,2,3]
-var bound = multiplyNum.bind(obj);
-console.log(bound(1,2,3));
+var student = {age : 20}
+var printAge = function(a){
+    return this.age
+}
+console.log(printAge.call(student,20))
+
+
 
