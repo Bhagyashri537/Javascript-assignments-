1de541a47e9dc0ff03fc91db76ca150f8c07ae45
Bhagyashri537 <bhagyashrideoghare116@gmail.com>
2022-12-19 12:49:01 +0530

    student placement

--- shree.js
+++ shree.js
@@ -1,38 +1,30 @@
-
-class Student {
-    static count = 0;
-    constructor(name,age,phone_no,marks){
+class student {
+    constructor(name, age, marks){
         this.name = name;
         this.age = age;
-        this.phone_no = phone_no;
         this.marks = marks;
-        Student.countStudents()
     }
-
-    static countStudents(){
-        return(Student.count++)
+ eligibleForPlacement(minAge){
+    console.log(this)
+    return (minMarks) => {
+        console.log(`eligibleForCompany`,this)
+        if(this.marks > minMarks && this.age > minAge){
+            console.log(`${this.name} is eligible for placement`)
+        }
+        else if(this.marks < minMarks && this.age < minAge){
+            console.log(`${this.name} is not eligible for placement`)
+        }
     }
-
-
-isEligible(){
-    if(this.marks >= 40){
-        console.log(`${this.name} is eligible`)
-    }
-    else if(this.marks<40){
-        console.log(`${this.name} is not eligible`)
-    }
-}
+ }
 }
 
-let obj1 = new Student('Ram', 15, 123415, 25)
-let obj2 = new Student('Pooja', 15, 123245, 48)
-let obj3 = new Student('Sam', 15, 123435, 55)
-let obj4 = new Student('Ravi', 15, 123445, 35)
-let obj5 = new Student('Rohan', 15, 123465, 75)
 
-console.log(Student.countStudents())
-obj1.isEligible();
-obj2.isEligible();
-obj3.isEligible();
-obj4.isEligible();
-obj5.isEligible();
+let obj1 = new student('Rahul',25, 78)
+let obj2 = new student('shree',20, 55)
+let obj3 = new student('sam',30, 75)
+
+
+
+obj1.eligibleForPlacement(21)(65);
+obj2.eligibleForPlacement(21)(65)
+obj3.eligibleForPlacement(21)(65)
