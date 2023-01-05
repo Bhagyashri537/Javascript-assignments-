e144d2ea4b96150477ade840ea42955ecd1c0f76
Bhagyashri537 <bhagyashrideoghare116@gmail.com>
2023-01-05 16:23:58 +0530

    1st file of axios

--- start.js
+++ start.js
@@ -1,114 +1,114 @@
 function getTodos() {
 
     console.log('GET Request');
-  
+
   }
-  
-  
-  
-  
+
+
+
+
   // POST REQUEST
-  
+
   function addTodo() {
-  
+
     console.log('POST Request');
-  
+
   }
-  
-  
-  
-  
+
+
+
+
   // PUT/PATCH REQUEST
-  
+
   function updateTodo() {
-  
+
     console.log('PUT/PATCH Request');
-  
+
   }
-  
-  
-  
-  
+
+
+
+
   // DELETE REQUEST
-  
+
   function removeTodo() {
-  
+
     console.log('DELETE Request');
-  
+
   }
-  
-  
-  
-  
+
+
+
+
   // SIMULTANEOUS DATA
-  
+
   function getData() {
-  
+
     console.log('Simultaneous Request');
-  
+
   }
-  
-  
-  
-  
+
+
+
+
   // CUSTOM HEADERS
-  
+
   function customHeaders() {
-  
+
     console.log('Custom Headers');
-  
+
   }
-  
-  
-  
-  
+
+
+
+
   // TRANSFORMING REQUESTS & RESPONSES
-  
+
   function transformResponse() {
-  
+
     console.log('Transform Response');
-  
+
   }
-  
-  
-  
-  
+
+
+
+
   // ERROR HANDLING
-  
+
   function errorHandling() {
-  
+
     console.log('Error Handling');
-  
+
   }
-  
-  
-  
-  
+
+
+
+
   // CANCEL TOKEN
-  
+
   function cancelToken() {
-  
+
     console.log('Cancel Token');
-  
+
   }
-  
-  
-  
-  
+
+
+
+
   // INTERCEPTING REQUESTS & RESPONSES
-  
-  
-  
-  
+
+
+
+
   // AXIOS INSTANCES
-  
-  
-  
-  
+
+
+
+
   // Show output in browser
-  
+
   function showOutput(res) {
-  
+
     document.getElementById('res').innerHTML = `
   
     <div class="card card-body mb-4">
@@ -172,32 +172,32 @@ function getTodos() {
     </div>
   
   `;
-  
+
   }
-  
-  
-  
-  
+
+
+
+
   // Event listeners
-  
+
   document.getElementById('get').addEventListener('click', getTodos);
-  
+
   document.getElementById('post').addEventListener('click', addTodo);
-  
+
   document.getElementById('update').addEventListener('click', updateTodo);
-  
+
   document.getElementById('delete').addEventListener('click', removeTodo);
-  
+
   document.getElementById('sim').addEventListener('click', getData);
-  
+
   document.getElementById('headers').addEventListener('click', customHeaders);
-  
+
   document
-  
+
     .getElementById('transform')
-  
+
     .addEventListener('click', transformResponse);
-  
+
   document.getElementById('error').addEventListener('click', errorHandling);
-  
+
   document.getElementById('cancel').addEventListener('click', cancelToken);
