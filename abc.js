40d04f5266b73dbf91412ef24406e878631e2250
Bhagyashri537 <bhagyashrideoghare116@gmail.com>
2023-01-05 16:15:25 +0530

    await1

--- abc.js
+++ abc.js
@@ -1,89 +1,37 @@
-const posts = [
+console.log('person1: shows tickets')
+console.log('person2: shows tickets')
 
-    {title: 'post one', body: 'this is post one', createdAt: new Date().getTime()},
-    {title: 'post two', body: 'this is post two', createdAt: new Date().getTime()}
+const preMovie = async() => {
+const promiseWifeBringingTicket = new Promise((resolve,reject) =>{
+    setTimeout(() => resolve('ticket'), 3000)
+})
 
-];
-let intervalId = 0;
+const getPopcorn = new Promise((resolve,reject) => resolve(`popcorn`));
+const getButter = new Promise((resolve,reject) => resolve(`butter`));
+const getColdDrink = new Promise((resolve,reject) => resolve(`colddrink`));
 
-function getPosts(){
-    clearInterval(intervalId)
-    intervalId = setInterval(() => {
+let ticket = await promiseWifeBringingTicket;
 
-       let output = '';
-       for(let i = 0; i<posts.length; i++) {
-        output += `<li>${posts[i].title} - last updated ${(new Date().getTime() - posts[i].createdAt)/1000}second ago</li>`
-       }
-      console.log('timer running id = ',intervalId)
-       document.body.innerHTML = output;
-    }, 1000);
+console.log('wife: i have the tickets')
+console.log('husband: we should go in')
+console.log('wife: no i am hungry')
+
+let popcorn = await getPopcorn;
+console.log('wife:i got some popcorn')
+console.log('husband:we should go in')
+console.log('wife: no I need butter on my popcorn')
+
+let butter = await getButter; 
+    console.log('wife: i got some butter on popcorn')
+    console.log('anything else')
+    console.log('wife: yeah one more thing')
+
+let colddrink = await getColdDrink;
+    console.log('husband: ok what you want')
+    console.log('wife: i nedd cold drink also')
+
+return ticket;
 }
-
-var timer;
-var count = 0;
-function lastEditedInSecondAgo(){
-    count = 0;
-    clearInterval(timer)
-    timer = setInterval(() => {
-        count++;
-        console.log(count);
-    }, 2000);
-}
-
-    function createPost(post){
-        return new Promise((resolve,reject) => {
-            setTimeout(() => {
-                posts.push({...post,createdAt: new Date().getTime()});
-               //callback();
-
-                const error = true;
-                if(!error){
-                    resolve();
-                }
-                else
-                {
-                    reject('error :something went wrong');
-                }
-
-            }, 2000)
-
-        })
-    }
-    createPost( {title: 'post three', body: 'this is post three'}).then(getPosts).catch(err => console.log(err))
-
-    function DeletePost(){
-        return new Promise((resolve,reject) => {
-            setTimeout(() => {
-                if(posts.length>0){
-                    const lastelement = posts.pop();
-                    resolve(lastelement)
-                }
-                else{
-                    reject('array is empty now')
-                }
-            }, 5000)
-        })
-    }
-
-
-
-
-
-    DeletePost()
-    .then((deleteElement) => {
-        console.log(`${deleteElement}`)
-              getPosts()
-              DeletePost().then(() =>{
-                getPosts()
-                DeletePost().then(() =>{
-                   getPosts()
-                    DeletePost().then(() =>{}) 
-                   .catch((err)=>{
-                    console.log('inside catch block',err)
-                   })
-                }).catch((err) => {})
-            }).catch((err) => {})
-        })
-
-.catch(err => console.log(err))
-
+preMovie().then((m) => console.log(`person3 shows ${m}`))
+console.log('person4: shows tickets')
+console.log('person5: shows tickets')
